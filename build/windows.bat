3288ff2fee84a27665064c345772482d38c19c96
Vitória <167592832+VickciV1102@users.noreply.github.com>
2025-09-27 17:18:30 -0300

    mudaças no arquivo windows.bat para compilar no windows.

--- build/windows.bat
+++ build/windows.bat
@@ -2,6 +2,12 @@ REM Apaga cache antigo do CMake
 del /F /Q CMakeCache.txt
 rmdir /S /Q CMakeFiles
 
+<<<<<<< HEAD
+REM Compila Cmake
+cmake .. -G "MinGW Makefiles"
+
+=======
+>>>>>>> 3bacb7a (adicionado as dlls para windows)
 REM Compila
 mingw32-make
 
